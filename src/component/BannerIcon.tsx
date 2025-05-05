bee2144133a836fa964e0a85059e67f2617a5776
priyankRaiyani19 <priyankraiyani19@gmail.com>
2025-05-05 14:43:35 +0530

    fix bug for deploye ment

---
+++ src/component/BannerIcon.tsx
@@ -0,0 +1,28 @@
+import classNames from "classnames";
+import { motion } from "framer-motion";
+
+import type { IconActionButtonProps } from "../lib/types/Types.ts";
+
+function BannerIcon({
+  isActive = false,
+  onClick,
+  activeClassName,
+  inactiveClassName,
+  icon,
+}: IconActionButtonProps) {
+  return (
+    <motion.button
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+      onClick={onClick}
+      className={classNames(
+        "p-3 rounded-full backdrop-blur-sm transition-colors",
+        isActive ? activeClassName : inactiveClassName,
+      )}
+    >
+      {icon}
+    </motion.button>
+  );
+}
+
+export default BannerIcon;
